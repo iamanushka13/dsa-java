d1f49c5a2d8ad172c8df07b8f3b180b506e84012
Anushka Kudu <167289244+iamanushka13@users.noreply.github.com>
2025-10-31 12:45:33 +0530

    Create SecondLargestElement.java

---
+++ 01_Arrays/SecondLargestElement.java
@@ -0,0 +1,11 @@
+int largest = Integer.MIN_VALUE;
+int secondLargest = Integer.MAX_VALUE;
+for(int i=0;i<arr.length;i++){
+  if(arr[i]>largest){
+    secondLargest = largest;
+    largest = arr[i];
+  }
+  else if(arr[i]>secondLargest && arr[i]!=largest){
+    secondLargest = arr[i];
+  }
+return secondLargest==Integer.MIN_VALUE?-1:secondLargest;
